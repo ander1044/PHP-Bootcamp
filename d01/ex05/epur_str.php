dbb16656c747fee11308c905ff33f51f79eae24d
Anda BEN <anben@c5r6s3.wethinkcode.co.za>
2019-10-11 10:41:40 +0200

    epur_str

--- d01/ex05/epur_str.php
+++ d01/ex05/epur_str.php
@@ -1,24 +1,6 @@
 #!/usr/bin/php
 <?php
- $counter = 0;
-// $my_str = trim($argv[1], " ");
-if($argc > 1)
-{
-    while($argv[$counter])
-    {
-        echo trim($argv[$counter]);
-        $counter++;
-    }
-}
 
-
-// while($counter < count($my_str))
-// {
-//     echo "$my_str[$counter]";
-//     $counter++;
-//     if($counter != count($my_str))
-//         echo " ";
-//     else
-//         echo "\n";
-// }
+$my_str = preg_replace('/[\s]+/', ' ', $argv[1]);
+echo trim($my_str);
 ?>
